03afc7d0ac14120846487509fd85b68b13dee7ac
yuya <pzgq5uh7@s.okayama-u.ac.jp>
2022-01-22 21:59:15 +0900

    initial commit

--- face_setup.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-# 'D:\MiniPupper\MiniPupper_Face'
-def face_setup(path):
-    cmd = f'{path[0:2]} && cd {path} && git add . && git commit -m "modified" && git push origin main'
-    print(cmd)
-    # subprocess.Popen(cmd, shell=True)
